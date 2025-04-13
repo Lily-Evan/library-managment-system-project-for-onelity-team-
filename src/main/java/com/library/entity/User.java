1059443cdc8c9aed8018d038dc65c3960b3f6ee9
Panagiota Grosdouli <109725921+Lily-Evan@users.noreply.github.com>
2025-04-13 13:40:40 +0300

    Update User.java

--- src/main/java/com/library/entity/User.java
+++ src/main/java/com/library/entity/User.java
@@ -32,41 +32,4 @@ public class User {
     private Set<Role> roles = new HashSet<>();
 }
 
-// src/main/java/com/library/entity/Book.java
-package com.library.entity;
 
-import lombok.Data;
-import javax.persistence.*;
-import javax.validation.constraints.NotBlank;
-import java.time.LocalDateTime;
-
-@Entity
-@Table(name = "books")
-@Data
-public class Book {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    @NotBlank
-    private String title;
-    
-    @NotBlank
-    private String author;
-    
-    private boolean available = true;
-    
-    @ManyToOne
-    private User borrowedBy;
-    
-    private LocalDateTime borrowedAt;
-    
-    private LocalDateTime dueDate;
-}
-
-// src/main/java/com/library/entity/Role.java
-package com.library.entity;
-
-public enum Role {
-    USER, ADMIN
-}
