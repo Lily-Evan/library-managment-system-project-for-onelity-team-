8210a0aeb3794a231f8bfc9ef7de103f1417f29b
Panagiota Grosdouli <109725921+Lily-Evan@users.noreply.github.com>
2025-04-13 11:06:09 +0300

    Update UserRepository.java

--- src/main/java/com/library/repository/UserRepository.java
+++ src/main/java/com/library/repository/UserRepository.java
@@ -1,20 +1,24 @@
 // src/main/java/com/library/repository/UserRepository.java
 package com.library.repository;
 
-import com.library.model.User;
+import com.library.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
+    boolean existsByEmail(String email);
 }
 
 // src/main/java/com/library/repository/BookRepository.java
 package com.library.repository;
 
-import com.library.model.Book;
+import com.library.entity.Book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Long> {
-    List<Book> findByTitleContainingOrAuthorContaining(String title, String author);
+    List<Book> findByTitleContainingOrAuthorContainingIgnoreCase(String title, String author);
+    List<Book> findByAvailable(boolean available);
+    List<Book> findByBorrowedById(Long userId);
 }
