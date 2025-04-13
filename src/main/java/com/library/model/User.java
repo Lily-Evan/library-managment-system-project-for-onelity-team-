ddce9d94d9338fd13c9435610e25cb7683f4e0b8
Panagiota Grosdouli <109725921+Lily-Evan@users.noreply.github.com>
2025-04-13 13:42:06 +0300

    Update User.java

--- src/main/java/com/library/model/User.java
+++ src/main/java/com/library/model/User.java
@@ -22,32 +22,4 @@ public class User {
     private Role role = Role.USER;
 }
 
-// src/main/java/com/library/model/Book.java
-package com.library.model;
 
-import lombok.Data;
-import javax.persistence.*;
-
-@Entity
-@Table(name = "books")
-@Data
-public class Book {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    private String title;
-    private String author;
-    private boolean available = true;
-    
-    @ManyToOne
-    @JoinColumn(name = "borrowed_by_id")
-    private User borrowedBy;
-}
-
-// src/main/java/com/library/model/Role.java
-package com.library.model;
-
-public enum Role {
-    USER, ADMIN
-}
